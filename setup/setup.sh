4cb737d90c480d5d5d78fcbd4ddfe4968c4f07bf
christina <christinaliu2020@gmail.com>
2023-08-13 15:07:33 -0700

    add

---
+++ setup/setup.sh
@@ -0,0 +1,14 @@
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt-get install python3.8
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 1
+sudo apt-get install unzip
+apt-get install python3-pip
+apt-get install rsync
+sudo apt-get install vim
+
+git clone https://github.com/christinaliu2020/ijepa.git
+
+#pip3 install git+https://github.com/huggingface/diffusers.git
+
+pip3 install -r requirements.txt
+
