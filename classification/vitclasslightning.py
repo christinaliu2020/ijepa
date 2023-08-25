a6e359b55782373b7bb1ad4536a7638c5a29865f
christina <christinaliu2020@gmail.com>
2023-08-24 20:37:52 -0700

    vitclass

--- classification/vitclasslightning.py
+++ classification/vitclasslightning.py
@@ -5,50 +5,65 @@ import torch.optim as optim
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 from torchvision.datasets import CIFAR10
+from torchvision.transforms import RandomResizedCrop
+
 from src.models.vision_transformer import VisionTransformer, vit_predictor, vit_small, MLP
 from src.helper import init_model, load_checkpoint, init_opt
 import matplotlib.pyplot as plt
 import pytorch_lightning as pl
 import wandb
-from lightning.pytorch.loggers import WandbLogger
+from lightning.pytorch.loggers.wandb import WandbLogger
 
 class IjepaCifar10(pl.LightningModule):
-    def __init__(self, checkpoint_path = '/home/christina/PycharmProjects/ijepa/logs/jepa-latest.pth.tar', num_classes=10, learning_rate=1e-3, track_wandb=True):
+    def __init__(self, checkpoint_path = None, num_classes=10, learning_rate=1e-3, track_wandb=True):
         super().__init__()
-        checkpoint = torch.load(checkpoint_path)
         self.encoder, self.predictor = init_model(
             device='cuda',
             patch_size=16,
-            model_name='vit_small', #changed from vit_base
+            model_name='vit_small',  # changed from vit_base
             crop_size=224,
-            pred_depth=6,
+            pred_depth=12,
             pred_emb_dim=384
         )
-        #self.target_encoder = copy.deepcopy(encoder)
-        def removeprefix(checkpointkey):
-            new_state_dict = {}
-            for key, value in checkpointkey.items():
-                if key.startswith('module.'):
-                    new_key = key[len('module.'):]
-                    new_state_dict[new_key] = value
-                else:
-                    new_state_dict[key] = value
-            return new_state_dict
+        if checkpoint_path is not None:
 
-        self.encoder.load_state_dict(removeprefix(checkpoint['encoder']))
+            checkpoint = torch.load(checkpoint_path)
 
-        def remove_keys_between_indices(dictionary, start_index, end_index):
-            new_dict = {k: v for i, (k, v) in enumerate(dictionary.items()) if i < start_index or i > end_index}
-            return new_dict
+            #self.target_encoder = copy.deepcopy(encoder)
+            def removeprefix(checkpointkey):
+                new_state_dict = {}
+                for key, value in checkpointkey.items():
+                    if key.startswith('module.'):
+                        new_key = key[len('module.'):]
+                        new_state_dict[new_key] = value
+                    else:
+                        new_state_dict[key] = value
+                return new_state_dict
 
-        pred_state_dict = removeprefix(checkpoint['predictor'])
-        new_pred_state_dict = remove_keys_between_indices(pred_state_dict, 76, 147)
-        self.predictor.load_state_dict(new_pred_state_dict)
+            self.encoder.load_state_dict(removeprefix(checkpoint['encoder']))
 
-        #self.target_encoder.load_state_dict(removeprefix(checkpoint['target_encoder']))
+            def remove_keys_between_indices(dictionary, start_index, end_index):
+                new_dict = {k: v for i, (k, v) in enumerate(dictionary.items()) if i < start_index or i > end_index}
+                return new_dict
+
+            pred_state_dict = removeprefix(checkpoint['predictor'])
+            new_pred_state_dict = remove_keys_between_indices(pred_state_dict, 76, 147)
+            #self.predictor.load_state_dict(new_pred_state_dict)
+
+        self.avg_pooling = nn.AdaptiveAvgPool1d(1)
         classification_head = nn.Linear(in_features=self.predictor.embed_dim, out_features=10)
+        classification_head = nn.Sequential(torch.nn.BatchNorm1d(self.predictor.embed_dim, affine=False, eps=1e-6),
+                                            classification_head)
         self.predictor = classification_head
+
+        # freeze all features except for class head
+        # for param in self.encoder.parameters():
+        #      param.requires_grad = False
+        #
+        # for param in classification_head.parameters():
+        #     param.requires_grad = True
         self.learning_rate = learning_rate
+
 
         self.track_wandb = track_wandb
         self.train_step_losses = []
@@ -60,24 +75,31 @@ class IjepaCifar10(pl.LightningModule):
 
     def forward(self,x):
         enc_output = self.encoder(x)
-        pred_output = self.predictor(enc_output)
-        pred_output_pooled = torch.mean(pred_output, dim=1)
-        return pred_output_pooled
+        enc_output = enc_output.permute(0, 2, 1)
+        pooled_output = self.avg_pooling(enc_output)
+        pooled_output = pooled_output.view(pooled_output.size(0), -1)
+        pred_output = self.predictor(pooled_output)
+
+        return pred_output
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
+        #optimizer = torch.optim.LARS
         return optimizer
 
     def training_step(self, batch, batch_idx):
         images, labels = batch
-        enc_output = self.encoder(images.to('cuda'))
-        pred_output = self.predictor(enc_output)
-        pred_output_pooled = torch.mean(pred_output, dim=1)
-        loss = nn.CrossEntropyLoss()(pred_output_pooled, labels)
+        # enc_output = self.encoder(images.to('cuda'))
+        # enc_output = enc_output.permute(0, 2, 1)
+        # pooled_output = self.avg_pooling(enc_output)
+        # pooled_output = pooled_output.view(pooled_output.size(0), -1)
+        # pred_output = self.predictor(pooled_output)
+        logits = self.forward(images)
+        loss = nn.CrossEntropyLoss()(logits, labels)
         self.train_step_losses.append(loss)
         # log accuracy
-        _, preds = torch.max(pred_output_pooled.data, dim=1)
-        acc = (preds == labels).sum().item() / pred_output_pooled.size(dim=0)
+        _, preds = torch.max(logits.data, dim=1)
+        acc = (preds == labels).sum().item() / logits.size(dim=0)
         acc *= 100
         self.train_step_acc.append(acc)
 
@@ -101,14 +123,18 @@ class IjepaCifar10(pl.LightningModule):
 
     def validation_step(self, batch, batch_idx):
         images, labels = batch
-        enc_output = self.encoder(images.to('cuda'))
-        pred_output = self.predictor(enc_output)
-        pred_output_pooled = torch.mean(pred_output, dim=1)
-        loss = nn.CrossEntropyLoss()(pred_output_pooled, labels)
+        # enc_output = self.encoder(images.to('cuda'))
+        # enc_output = enc_output.permute(0, 2, 1)
+        # pooled_output = self.avg_pooling(enc_output)
+        # pooled_output = pooled_output.view(pooled_output.size(0), -1)
+        # pred_output = self.predictor(pooled_output)
+        # pred_output_pooled = torch.mean(pred_output, dim=1)
+        logits = self.forward(images)
+        loss = nn.CrossEntropyLoss()(logits, labels)
         self.val_step_losses.append(loss)
         # log accuracy
-        _, preds = torch.max(pred_output_pooled.data, dim=1)
-        acc = (preds == labels).sum().item() / pred_output_pooled.size(dim=0)
+        _, preds = torch.max(logits.data, dim=1)
+        acc = (preds == labels).sum().item() / logits.size(dim=0)
         acc *= 100
         self.val_step_acc.append(acc)
 
@@ -142,24 +168,35 @@ class IjepaCifar10(pl.LightningModule):
 
 if __name__ == "__main__":
     # pl.seed_everything(42)
-
-
+    #checkpoint= '/home/christina/PycharmProjects/ijepa/logs/jepa-latest.pth.tar'
+    checkpoint = None
     wandb.init(project="ijepa-classification")
     wandb_logger = WandbLogger(project="ijepa-classification")
 
-    transform = transforms.Compose([
-        transforms.Resize((224, 224)),
+    # transform = transforms.Compose([
+    #     transforms.Resize((224, 224)),
+    #     transforms.RandomHorizontalFlip(),
+    #     transforms.ToTensor(),
+    #     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    # ])
+    transform_train = transforms.Compose([
+        RandomResizedCrop(224, interpolation=3),
+        transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
-        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-    ])
-    train_dataset = CIFAR10(root="./data", train=True, download=True, transform=transform)
-    val_dataset = CIFAR10(root="./data", train=False, download=True, transform=transform)
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+    transform_val = transforms.Compose([
+        transforms.Resize(256, interpolation=3),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+    train_dataset = CIFAR10(root="./data", train=True, download=True, transform=transform_train)
+    val_dataset = CIFAR10(root="./data", train=False, download=True, transform=transform_val)
 
     train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=32, shuffle=True, num_workers=4)
     val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=32, shuffle=False, num_workers=4)
 
 
-    model = IjepaCifar10()
+    model = IjepaCifar10( checkpoint_path = checkpoint)
 
 
     trainer = pl.Trainer(num_nodes=1, max_epochs=50, logger=wandb_logger)
